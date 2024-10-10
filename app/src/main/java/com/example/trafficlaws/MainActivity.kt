f0d569960639c77568a9271ef364714b85b7c86e
maks2001916 <mamonov.maksim.20003@gmail.com>
2024-10-10 20:47:18 +0500

    завершена вёрстка и прописана логика приложения

--- app/src/main/java/com/example/trafficlaws/MainActivity.kt
+++ app/src/main/java/com/example/trafficlaws/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.trafficlaws
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var informationET: EditText
+    private lateinit var checkBoxCB: CheckBox
+    private lateinit var textTV: TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +25,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        informationET = findViewById(R.id.infoET)
+        checkBoxCB = findViewById(R.id.checkboxCB)
+        textTV = findViewById(R.id.infoET)
+        checkBoxCB.setOnCheckedChangeListener {buttonView, isChecked ->
+            if (checkBoxCB.isChecked) {
+                informationET.text.clear()
+                informationET.setText(R.string.pdd)
+                textTV.setText(getString(R.string.Traffic_Laws))
+            }
+            else {
+                informationET.setText(R.string.info)
+                textTV.setText("")
+            }
         }
     }
 }
